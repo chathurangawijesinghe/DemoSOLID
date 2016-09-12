1842f3ce3eda9651b2342004057de58ec953f225
chathurangarw <chathurangarw@zone24x7.com>
2016-09-12 08:59:35 +0530

    Single responsibility principle

---
+++ DemoSOLID/Classes/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoSOLID.Classes
+{
+    public class Customer
+    {
+        private FileLogger logger = new FileLogger();
+
+        public void Add()
+        {
+            try
+            {
+                // Database code goes here
+            }
+            catch (Exception ex)
+            {
+                logger.Handle(ex.ToString());
+            }
+        }
+    }
+}
---
+++ DemoSOLID/Classes/FileLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoSOLID.Classes
+{
+    public class FileLogger
+    {
+        public void Handle(string error)
+        {
+            System.IO.File.WriteAllText(@"log.txt", error);
+        }
+    }
+}
